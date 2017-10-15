1ca7bd69f0763dc27101ed030c8f03aa7ea433d3
egwuenu gift <laurandidi21@gmail.com>
2017-10-15 08:53:31 +0100

    initial commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,23 @@
-console.log('It works.');
+new Vue ({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            { title: 'item 1'},
+            { title: 'item 2'},
+            { title: 'item 3'}
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function(index){
+           this.total += 9.99;
+           var item = this.items[index];
+           this.cart.push({
+               title: '',
+               qty: 1
+           });   
+        }
+    }
+
+});

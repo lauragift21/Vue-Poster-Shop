e617de47d3869b14773bce1f2095f07eaf13c28e
egwuenu gift <laurandidi21@gmail.com>
2017-10-21 12:28:50 +0100

    added cart buttons

--- public/script.js
+++ public/script.js
@@ -1,23 +1,43 @@
+var PRICE = 9.99;
+
 new Vue ({
     el: '#app',
     data: {
         total: 0,
         items: [
-            { title: 'item 1'},
-            { title: 'item 2'},
-            { title: 'item 3'}
+            { id: 1, title: 'item 1'},
+            { id: 2, title: 'item 2'},
+            { id: 3, title: 'item 3'}
         ],
         cart: []
     },
     methods: {
-        addItem: function(index){
-           this.total += 9.99;
-           var item = this.items[index];
-           this.cart.push({
-               title: '',
-               qty: 1
-           });   
+        addItem: function(index) {
+           this.total += PRICE;
+           var item = this.items[index-1];
+           var found = false;
+           for(var i = 0; i < this.cart.length; i++) {
+               if(this.cart[i].id === item) {
+                found = true;
+                   this.cart[i].qty++;
+               }
+           }
+           if(!found) {
+            this.cart.push({
+                id: item,
+                title: item,
+                qty: 1,
+                price: PRICE
+            });
+            }
+        },
+    },
+
+    filters: {
+        currency: function(price){
+            return '$'.concat(price.toFixed(2));
         }
     }
+
 
 });
